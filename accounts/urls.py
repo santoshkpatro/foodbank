cccd777f6fed6e6160dc387c75bc326dbdedb693
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2022-04-29 23:31:02 +0530

    login logout is done

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
 
 
 def login_view(request):
+    if request.method == 'POST':
+        sic = request.POST['sic']
+        password = request.POST['password']
+        user = authenticate(request, sic=sic, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.warning(request, 'Invalid credentials')
     return render(request, 'accounts/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Logout success')
+    return redirect('index')
